297766cbc02b1b25bf588e42695c312320eefe0b
Choromi Tao <princessailish94@mail.ru>
2023-06-18 00:03:08 +0900

    The third lession

--- src/Lession_4_1.java
+++ src/Lession_4_1.java
@@ -4,54 +4,116 @@ import java.util.Scanner;
 
 public class Lession_4_1 {
     public static void main(String[] args) {
-        String[] myStr;
-        ArrayList<String[]> arrayStrings = new ArrayList<>();
+        ArrayList<String> lastNames = new ArrayList<>();
+        ArrayList<String> firstNames = new ArrayList<>();
+        ArrayList<String> surNames = new ArrayList<>();
         ArrayList<Integer> ages = new ArrayList<>();
-        // Существет проблемма с русскими символами в Scanner
+        ArrayList<String> genders = new ArrayList<>();
+        // Существет проблема с русскими символами в Scanner
         String encoding = System.getProperty("console.encoding", "UTF-8");
         Scanner scanner = new Scanner(System.in, encoding);
 
-        while (true) {
-            System.out.println("-".repeat(20));
-            System.out.println(
-                    "Введите строки в формате:\nФамилия Имя Отчество Возраст Пол\nЧтобы закончить ввод данных - введите пустую строку.");
-            System.out.println("Введите строку: ");
-            myStr = GetArrayString(scanner.nextLine());
+        String[] inputFieldNames = new String[]{"ФИО", "Пол", "Возраст"};
+        String[] inputFields = new String[inputFieldNames.length];
 
-            if (myStr == null)
-                break;
-//            if (myStr[0].equals(""))
-//                break;          // выход из ввода данных вовзращаем null;
-            arrayStrings.add(myStr);
-            ages.add(Integer.parseInt(myStr[3]));
-            //
+        boolean input = true;
+        boolean add;
+        while (input) {
+            add = true;
 
-        }
-        PrintArray(arrayStrings, -1);
-        scanner.close();
-    }
-//        public static ArrayList<Integer> minimal_maximal_indexes(ArrayList<String[]> arrayList, int sortrby){
-//
-//            return new_ages;
-//        }
-        public static void PrintArray(ArrayList<String[]> arrayList, int sortby){
-            arrayList.sort(new Comparator<String[]>() {
-                @Override
-                public int compare(String[] o1, String[] o2) {
-                    int r =  Integer.parseInt(o1[3]) - Integer.parseInt(o2[3]);
-                    return r*sortby;
+            for (int i = 0; i < inputFields.length; i++) {
+//                проверяем ввод пустой строки
+                do {
+                    inputFields[i] = inputData(scanner, inputFieldNames[i]);
+                } while (inputFields[i].length() == 0);
+
+//                выход или вывод
+                if (inputFields[i].equals("exit")) {
+                    input = false;
+                    add = false;
+                    break;
+                } else if (inputFields[i].equals("show")) {
+                    add = false;
+                    System.out.println();
+                    showPersons(lastNames, firstNames, surNames, ages, genders);
+                    System.out.println();
+                    showSortPersons(lastNames, firstNames, surNames, ages, genders);
+                    break;
                 }
-            });
-            for (String[] myStr: arrayList) {
-                System.out.println(String.format("%s %s. %s. %s %s",myStr[0], myStr[1].charAt(0), myStr[2].charAt(0), myStr[3],myStr[4]));
+            }
+
+            if (add) {
+                String[] fullnameArr = inputFields[0].split(" ");
+                lastNames.add(fullnameArr[0]);
+                firstNames.add(fullnameArr[1]);
+                surNames.add(fullnameArr[2]);
+                genders.add(inputFields[1]);
+
+                if (isDigit(inputFields[2])) {
+                    ages.add(Integer.parseInt(inputFields[2]));
+                } else {
+                    ages.add(0);
+                }
             }
         }
-        public static String[] GetArrayString(String str) {
-            String[] result = null;
-            result = str.strip().split(" ");
-                if (result.length != 5) {
-                    return null;
-                }
-            return result;
+    }
+    public static void showPersons(ArrayList<String> lastNames, ArrayList<String> firstNames, ArrayList<String> surNames,
+                                   ArrayList<Integer> ages, ArrayList<String> genders) {
+        for (int i = 0; i < lastNames.size(); i++) {
+            System.out.println(lastNames.get(i) + " " + firstNames.get(i).charAt(0) + "." + surNames.get(i).charAt(0) + ". "
+                    + ages.get(i) + " " + genders.get(i));
         }
+    }
+
+    public static void showSortPersons(ArrayList<String> lastNames, ArrayList<String> firstNames, ArrayList<String> surNames,
+                                       ArrayList<Integer> ages, ArrayList<String> genders) {
+        int[] indexes = getSortIndexes(ages, genders);
+
+        for (int index : indexes) {
+            System.out.println(lastNames.get(index) + " " + firstNames.get(index).charAt(0) + "." + surNames.get(index).charAt(0) + ". "
+                    + ages.get(index) + " " + genders.get(index));
+        }
+    }
+
+    public static String inputData(Scanner scanner, String text) {
+        System.out.print(text + ": ");
+        if (scanner.hasNext())
+            return scanner.nextLine();
+        else
+            return "";
+    }
+
+    public static int[] getSortIndexes(ArrayList<Integer> ages, ArrayList<String> genders) {
+        int[] indexes = new int[ages.size()];
+        for (int i = 0; i < ages.size(); i++) {
+            indexes[i] = i;
+        }
+
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < indexes.length - 1; i++) {
+//              либо по полу, если они разные и не в том порядке
+//              либо по возрасту, если пол один
+                if ((genders.get(indexes[i]).equals("Женский") && genders.get(indexes[i + 1]).equals("Мужской"))
+                        || ((genders.get(indexes[i]).equals(genders.get(indexes[i + 1]))) && (ages.get(indexes[i]) > ages.get(indexes[i + 1])))) {
+                    isSorted = false;
+                    int tmp = indexes[i];
+                    indexes[i] = indexes[i + 1];
+                    indexes[i + 1] = tmp;
+                }
+            }
+        }
+
+        return indexes;
+    }
+
+    private static boolean isDigit(String str) throws NumberFormatException {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
